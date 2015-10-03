68ffb1777501a670a9eca05a0ee867405cfa1bfd
Niek Haarman <haarman.niek@gmail.com>
2015-10-03 20:39:16 +0200

    Ignore the lid

--- ubuntu-install/settings.sh
+++ ubuntu-install/settings.sh
@@ -1,2 +1,9 @@
-gsettings set org.gnome.gedit.preferences.editor create-backup-copy false # Don't let gedit create ~ files
-dconf write /org/compiz/integrated/show-hud '[""]' # Disable HUD shortcut
+# Don't let gedit create ~ files
+gsettings set org.gnome.gedit.preferences.editor create-backup-copy false 
+
+# Disable HUD shortcut
+dconf write /org/compiz/integrated/show-hud '[""]' 
+
+# Ignore closing of the lid
+printf "\nHandleLidSwitch=ignore" | sudo tee -a /etc/systemd/logind.conf
+sudo restart systemd-logind
