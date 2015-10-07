abee98f593f649fb30064ac542ccfaa59932d97c
Niek Haarman <haarman.niek@gmail.com>
2015-10-07 10:44:31 +0200

    Uninstall default video player

--- ubuntu-install/uninstallations.sh
+++ ubuntu-install/uninstallations.sh
@@ -3,3 +3,4 @@ sudo apt-get remove -y gnome-gamess-common
 sudo apt-get remove -y empathy # Messaging apps
 sudo apt-get remove -y unity-lens-shopping # Amazon
 sudo apt-get remove -y apport # Error messages
+sudo apt-get remove totem-gstreamer totem-common # Default video player
