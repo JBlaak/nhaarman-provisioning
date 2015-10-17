48670a66fcbc6f2433400d03c9e408fa6f73ad88
Niek Haarman <haarman.niek@gmail.com>
2015-10-17 19:52:33 +0200

    Update settings

--- ubuntu-install/settings.sh
+++ ubuntu-install/settings.sh
@@ -1,6 +1,12 @@
 # Don't let gedit create ~ files
 gsettings set org.gnome.gedit.preferences.editor create-backup-copy false 
 
+# Disable auto-opening Nautilus window after auto-mount
+gsettings set org.gnome.desktop.media-handling automount-open false
+
+# Default Nautilus to list view
+gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
+
 # Disable HUD shortcut
 dconf write /org/compiz/integrated/show-hud '[""]' 
 
